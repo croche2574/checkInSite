d6351fa461ead201d8c162d4dbe75f56f9cc08f4
croche2574 <croche2574@flpoly.org>
2016-10-22 16:46:48 -0400

    commit test

---
+++ main.py
@@ -0,0 +1,39 @@
+import tornado.ioloop
+import tornado.web
+import tornado.httpserver
+import os
+import pymysql
+
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('static/index.html')
+
+class DashHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('static/dashboard.html')
+
+class Application(tornado.web.Application): 
+    def __init__(self):
+
+        
+
+        handlers = [
+            (r'/Dashboard', DashHandler),
+            (r'/(.*)', tornado.web.StaticFileHandler, {'path': "static"}),
+            
+            (r'/', IndexHandler),
+        ]
+        settings = {
+            "debug": True,
+            
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+
+if __name__ == "__main__":
+    server = tornado.httpserver.HTTPServer(Application())
+    server.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
